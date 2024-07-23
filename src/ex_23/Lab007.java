8ed27ce09c56503318f4e06a345e42947011e963
Sneha Rathoe <rathoresneha22@gmail.com>
2024-07-23 21:05:52 +0530

    new files

---
+++ src/ex_23/Lab007.java
@@ -0,0 +1,7 @@
+package ex_23;
+
+public class Lab007 {
+    public static void main(String[] args){
+        System.out.println(9-99);
+    }
+}
---
+++ src/ex_23/Lab008.java
@@ -0,0 +1,7 @@
+package ex_23;
+
+public class Lab008 {
+    public static void main(String[]args){
+        System.out.println(77+2);
+    }
+}
