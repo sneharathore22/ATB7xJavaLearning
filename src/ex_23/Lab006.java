36403480f9a2530ff70de546c8ee88bd3edfacee
Sneha Rathoe <rathoresneha22@gmail.com>
2024-07-23 21:13:02 +0530

    new files

--- src/ex_23/Lab006.java
+++ src/ex_23/Lab006.java
@@ -2,7 +2,7 @@ package ex_23;
 
 public class Lab006 {
     public static void main(String[] args){
-        System.out.println(7+88);
+        System.out.println(7+88-45);
     }
 
 }
---
+++ src/ex_23/Lab010.java
@@ -0,0 +1,7 @@
+package ex_23;
+
+public class Lab010 {
+    public static void main(String[]args){
+        System.out.println("Happy New Year");
+    }
+}
