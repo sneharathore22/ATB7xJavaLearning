4e9da4908fce5ab967b377163c96f5d1f960a15c
Sneha Rathoe <rathoresneha22@gmail.com>
2024-08-03 20:44:01 +0530

    Array

---
+++ src/ex_0308/Lab151.java
@@ -0,0 +1,23 @@
+package ex_0308;
+
+public class Lab151 {
+    public static void main(String[] args) {
+
+        int [][]array_2d ={
+                {66,55},
+                {25,35},
+                {89,35}
+
+        };
+
+        int row = array_2d.length;
+        for(int i =0; i<row; i++){
+            for (int j =0; j< array_2d[i].length;j++){
+                System.out.print(array_2d[i][j]);
+                System.out.print("\t");
+            }
+
+            System.out.println("");
+        }
+    }
+}
