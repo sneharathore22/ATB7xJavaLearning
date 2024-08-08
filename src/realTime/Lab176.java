9bf0358c26d6a3acbb52e110fcf33840c798c9af
Sneha Rathoe <rathoresneha22@gmail.com>
2024-08-08 13:17:44 +0530

    java

---
+++ src/realTime/Lab176.java
@@ -0,0 +1,12 @@
+package realTime;
+
+public class Lab176 {
+        //Bank, ICICI ->store Information
+        // Bank and ICICI -> is A - icici -bank
+        //Bank has customer - hsA
+        //Programing Language Python, Java -> isA
+
+        //Laptop ,Keyboard, mouse, camera  -> hsA
+
+        //IsA or hsA -> Aggrestio
+}
